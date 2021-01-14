7c89c0be71cbc3c6e590d3ec7c184435ff2d251f
Florence <monnaie.florence@gmail.com>
2021-01-14 16:06:59 +0100

    Fin exo10

--- src/App.js
+++ src/App.js
@@ -8,15 +8,35 @@ class App extends Component {
   };
 
   changeParagraphe = (e) => {
+
+    // let para = document.querySelector('p');
+    // para.textContent += 'nouveau Mot de la fin';
     e.target.previousSibling.textContent += 'Mot de la fin.'
   };
+
+  change2ndTitle = (e) => {
+    let newtitle = e.target.parentElement.parentElement.querySelector('h3');
+    newtitle.textContent = e.target.previousSibling.value;
+  };
+
+  change2ndParagraphe = (e) => {
+    e.target.previousSibling.textContent = e.target.value;
+  };
+
 
   render() {
     return (
       <div>
         <h1 onClick={(e)=> this.changeTitle(e,'Nouveau joli Titre')}>Joli Titre</h1>
+        <h3>Joli sous-titre</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ex unde ut reprehenderit, cumque labore! Possimus omnis est ea excepturi!</p>
         <button onClick={this.changeParagraphe}>Modifier le paragraphe</button>
+        <div>
+          <input type="text"/>
+          <input type="submit" value="Sous-titre" onClick={this.change2ndTitle}/>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, repellendus!</p>
+        <textarea name="change" id="change" cols="30" rows="10" onChange={this.change2ndParagraphe}></textarea>
       </div>
     )
   }
